6e3b04522f498510eecce26e9af69f39c54c3e9b
chuyxuan_mbp <mail@chuyuxuan.top>
2024-02-12 20:28:24 +0800

    init: 初始化

---
+++ src/pages/my/my.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="my">
+    myfwjifiowjfwei
+
+    <uni-icons type="contact" size="30"></uni-icons>
+
+  </div>
+</template>
+
+<script lang="ts" setup>
+const bindClick = () =>{
+  console.log('click');
+  
+}
+</script>
+
+<style scoped></style>
