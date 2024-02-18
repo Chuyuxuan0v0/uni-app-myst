ab5f41aa4e51113da58281b6283c4015846e948f
chuyxuan_mbp <mail@chuyuxuan.top>
2024-02-18 13:41:07 +0800

    init: 初始化模板

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -1,8 +1,28 @@
-<template>
-  <div class="inder">myyyyy</div>
-</template>
+<script setup lang="ts">
+import { useMemberStore } from '@/stores'
 
-<script lang="ts" setup>
+const memberStore = useMemberStore()
 </script>
 
-<style scoped></style>
+<template>
+  <view class="my">
+    <view>会员信息：{{ memberStore.profile }}</view>
+    <button
+      @tap="
+        memberStore.setProfile({
+          nickname: '黑马先锋',
+        })
+      "
+      size="mini"
+      plain
+      type="primary"
+    >
+      保存用户信息
+    </button>
+    <button @tap="memberStore.clearProfile()" size="mini" plain type="warn">清理用户信息</button>
+  </view>
+</template>
+
+<style lang="scss">
+//
+</style>
--- src/stores/modules/member.ts
+++ src/stores/modules/member.ts
@@ -1,4 +1,3 @@
-import type { LoginResult } from '@/types/member'
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
@@ -7,10 +6,10 @@ export const useMemberStore = defineStore(
   'member',
   () => {
     // 会员信息
-    const profile = ref<LoginResult>()
+    const profile = ref<any>()
 
     // 保存会员信息，登录时使用
-    const setProfile = (val: LoginResult) => {
+    const setProfile = (val: any) => {
       profile.value = val
     }
 
@@ -26,19 +25,8 @@ export const useMemberStore = defineStore(
       clearProfile,
     }
   },
+  // TODO: 持久化
   {
-    // 网页端配置
-    // persist: true,
-    // 小程序端配置
-    persist: {
-      storage: {
-        getItem(key) {
-          return uni.getStorageSync(key)
-        },
-        setItem(key, value) {
-          uni.setStorageSync(key, value)
-        },
-      },
-    },
+    persist: true,
   },
 )
